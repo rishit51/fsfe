71e7e1d48cd2111ff3a5de2469e10928e2151d75
Rishit Srivastava <ui21cs51@iiitsurat.ac.in>
2023-10-06 18:49:17 +0530

    Added databse'

--- index-ws.js
+++ index-ws.js
@@ -8,22 +8,40 @@ app.get("/", function (req, res) {
 
 server.on("request", app);
 server.listen(3000, function () {
-  console.log("Listening on port 3000");
+  console.log("server started on port 3000");
 });
 
-/* ******Websocket Begin */
-const WebsocketServer = require("ws").Server;
+/** Begin websocket */
+const WebSocketServer = require("ws").Server;
 
-const wss = new WebsocketServer({ server: server });
+const wss = new WebSocketServer({ server: server });
+
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
 
 wss.on("connection", function connection(ws) {
-  const numclients = wss.clients.size;
-  console.log("Clients connected:", numclients);
-  wss.broadcast(`Current visitors ${numclients}`);
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
   if (ws.readyState === ws.OPEN) {
     ws.send("Welcome to my server");
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
   ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
   });
 });
@@ -33,3 +51,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** end websockets */
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("Shutting down db");
+
+  getCounts();
+  db.close();
+}
