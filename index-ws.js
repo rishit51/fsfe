c23b532492513205b095c7fb355710ba1360bb1a
Rishit Srivastava <ui21cs51@iiitsurat.ac.in>
2023-10-06 16:48:25 +0530

    Websocket implemented

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,26 @@ server.on("request", app);
 server.listen(3000, function () {
   console.log("Listening on port 3000");
 });
+
+/* ******Websocket Begin */
+const WebsocketServer = require("ws").Server;
+
+const wss = new WebsocketServer({ server: server });
+
+wss.on("connection", function connection(ws) {
+  const numclients = wss.clients.size;
+  console.log("Clients connected:", numclients);
+  wss.broadcast(`Current visitors ${numclients}`);
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+  ws.on("close", function close() {
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
